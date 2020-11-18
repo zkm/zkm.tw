1ce4ab0c4f2af9951b8463485fd7560e141fae4b
Zach Schneider <me@zachschneider.com>
2020-11-18 12:24:26 -0600

    React App (#1)
    * init
    
    * Updating content to what I want
    
    * finished up a few of the basics, TODO: Add GA
    
    * Added tests

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.scss';
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h1>Zach Schneider [dot]com</h1>
+				<img src="https://zkm.s3.amazonaws.com/profile-coffee.jpg" alt="Zach Schneider" />
+				<h2>Stay in Touch</h2>
+				<ul>
+					<li>
+						<a className="App-link" href="https://twitter.com/zkm" title="Twitter">
+							Twitter
+						</a>
+					</li>
+
+					<li>
+						<a className="App-link" href="https://github.com/zkm" title="Github">
+							Github
+						</a>
+					</li>
+					<li>
+						<a className="App-link" href="/blog/" title="blog">
+							Blog
+						</a>
+					</li>
+				</ul>
+				&copy; 2006 — {new Date().getFullYear()}, ZachSchneider[dot]com
+			</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/App.test.js
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Twitter link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Twitter/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Github link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Github/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders Blog link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Blog/i);
+  expect(linkElement).toBeInTheDocument();
+});
