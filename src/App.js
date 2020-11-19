f003dad06f995fabe9dcecd77d1703cd012ebe0a
Zach Schneider <me@zachschneider.com>
2020-11-19 11:42:21 -0600

    Added "modern" images

--- src/App.js
+++ src/App.js
@@ -5,22 +5,27 @@ function App() {
 		<div className="App">
 			<header className="App-header">
 				<h1>Zach Schneider [dot]com</h1>
-				<img src="https://zkm.s3.amazonaws.com/profile-coffee.jpg" alt="Zach Schneider" />
+				<picture>
+					<source srcSet={process.env.PUBLIC_URL + './img/profile-coffee.webp'} type="image/webp" />
+					<source srcSet={process.env.PUBLIC_URL + './img/profile-coffee.jpg'} type="image/jpeg" />
+					<source srcSet={process.env.PUBLIC_URL + './img/profile-coffee.png'} type="image/png" />
+					<img src={process.env.PUBLIC_URL + './img/profile-coffee.png'} alt="Zach Schneider" />
+				</picture>
 				<h2>Stay in Touch</h2>
 				<ul>
 					<li>
-						<a className="App-link" href="https://twitter.com/zkm" title="Twitter">
+						<a className="App-link" role="button" href="https://twitter.com/zkm" title="Twitter">
 							Twitter
 						</a>
 					</li>
 
 					<li>
-						<a className="App-link" href="https://github.com/zkm" title="Github">
+						<a className="App-link" role="button" href="https://github.com/zkm" title="Github">
 							Github
 						</a>
 					</li>
 					<li>
-						<a className="App-link" href="/blog/" title="blog">
+						<a className="App-link" role="button" href="/blog/" title="blog">
 							Blog
 						</a>
 					</li>
