cd1e093a6391cd52cbb528f1f0a9f9af06b911cd
Zach Schneider <me@zachschneider.com>
2020-11-19 21:48:46 -0600

    image reference

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ function App() {
 			<header className="App-header">
 				<h1>Zach Schneider [dot]com</h1>
 				<picture>
-					<source srcSet={process.env.PUBLIC_URL + './img/profile-coffee.webp'} type="image/webp" />
-					<source srcSet={process.env.PUBLIC_URL + './img/profile-coffee.jpg'} type="image/jpeg" />
-					<source srcSet={process.env.PUBLIC_URL + './img/profile-coffee.png'} type="image/png" />
-					<img src={process.env.PUBLIC_URL + './img/profile-coffee.png'} alt="Zach Schneider" />
+					<source srcSet={process.env.PUBLIC_URL + '/img/profile-coffee.webp'} type="image/webp" />
+					<source srcSet={process.env.PUBLIC_URL + '/img/profile-coffee.jpg'} type="image/jpeg" />
+					<source srcSet={process.env.PUBLIC_URL + '/img/profile-coffee.png'} type="image/png" />
+					<img src={process.env.PUBLIC_URL + '/img/profile-coffee.png'} alt="Zach Schneider" />
 				</picture>
 				<h2>Stay in Touch</h2>
 				<ul>
