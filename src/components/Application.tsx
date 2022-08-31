02d91adb0d2ebda6ea7f45f83dd4007058cd56a1
Zach Schneider <me@zachschneider.com>
2022-08-31 12:04:09 -0500

    Upgraded packages, typescript, webpack, and all of the fun stuff

---
+++ src/components/Application.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import { hot } from "react-hot-loader";
+import logo from "@assets/images/logo.png";
+// import logo from '@assets/images/profile-coffee.webp';
+import webSafelogo from "@assets/images/profile-coffee.png";
+// import logo from '@assets/images/profile-coffee.webp';
+
+import "./Application.scss";
+
+type Props = {
+  title: string;
+  version: string;
+};
+
+const Application: React.FC<Props> = props => {
+  // const [counter, setCounter] = useState(0);
+
+  return (
+    <React.Fragment>
+      <main>
+        <div className="main-heading">
+          <h1>
+            {props.title}
+          </h1>
+        </div>
+        <p className="main-logo">
+          <img
+            src={logo}
+            alt="Zach Schneider"
+            srcSet={webSafelogo}
+            width="500"
+          />
+        </p>
+
+        <h2>Stay in Touch</h2>
+        <div className="main-connect">
+          <ul>
+          <li>
+              <a
+                className="btn"
+                role="button"
+                href="https://zachschneider.com/"
+                title="Blog">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a
+                className="btn"
+                role="button"
+                href="https://github.com/zkm"
+                title="Github">
+                Github
+              </a>
+            </li>
+            <li>
+              <a
+                className="btn"
+                role="button"
+                href="https://linkedin.com/in/zschneider"
+                title="LinkedIn">
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                className="btn"
+                role="button"
+                href="https://twitter.com/zkm"
+                title="Twitter">
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
+        <footer>
+          <sup>&copy;</sup> 2005 &ndash; {new Date().getFullYear()}
+        </footer>
+      </main>
+    </React.Fragment>
+  );
+};
+
+export default hot(module)(Application);
---
+++ tools/webpack/webpack.plugins.js
@@ -0,0 +1,16 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = [
+  new ForkTsCheckerWebpackPlugin(),
+  new HtmlWebpackPlugin({
+    template: 'src/index.html',
+    favicon: 'assets/images/favicon.ico',
+    inject: true,
+  }),
+  new MiniCssExtractPlugin({
+    filename: '[name].[chunkhash].css',
+    chunkFilename: '[name].[chunkhash].chunk.css',
+  }),
+];
