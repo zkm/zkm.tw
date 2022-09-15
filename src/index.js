0012dcd2aef1cf1850270bcc9ba58e0098e7d040
Zach Schneider <me@zachschneider.com>
2022-09-15 15:06:27 -0500

    Adding Google Analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ReactGA from 'react-ga';
+
+const TRACKING_ID = 'G-2J7SWPGLE4';
+ReactGA.initialize(TRACKING_ID);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
