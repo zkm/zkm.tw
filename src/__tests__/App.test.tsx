ee13e9de45aa18c8538934247d2b394d796e22b5
Zach Schneider <me@zachschneider.com>
2022-12-31 14:57:49 -0600

    Production build

--- src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders my name', () => {
-  render(<App gtmId="GTM-KTW7DXZ" />);
-  const titleElement: HTMLElement = screen.getByText(/Zach Schneider/);
-  expect(titleElement).toBeInTheDocument();
-});
