b6350ecb2f08ba3f0348b866499f3c82789da51f
Zach Schneider <me@zachschneider.com>
2025-08-11 18:47:10 -0500

    Updated deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   test: {
     globals: true,
     environment: 'jsdom',
