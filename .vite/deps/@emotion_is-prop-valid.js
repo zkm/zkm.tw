58d547391f824c80d5ef66b2f7509c4438d4a16f
Zach Schneider <me@zachschneider.com>
2025-08-14 15:15:10 -0500

    Update production build for deployment

--- .vite/deps/@emotion_is-prop-valid.js
+++
@@ -1,9 +0,0 @@
-import {
-  init_emotion_is_prop_valid_esm,
-  isPropValid
-} from "./chunk-USFJTKGU.js";
-import "./chunk-V4OQ3NZ2.js";
-init_emotion_is_prop_valid_esm();
-export {
-  isPropValid as default
-};
--- .vite/deps/react.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react
-} from "./chunk-K4NLKXWU.js";
-import "./chunk-V4OQ3NZ2.js";
-export default require_react();
