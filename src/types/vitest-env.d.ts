6873e029fe58cb2391db6c659e5fa98492052f47
Zach Schneider <me@zachschneider.com>
2025-10-03 11:43:16 -0500

    fixed tests

---
+++ src/types/vitest-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="vitest" />
+/// <reference types="@testing-library/jest-dom" />
+
+// Having this file ensures the editor loads Vitest and Testing Library types
+// for files under `src/**`, including `src/__tests__/**`.
