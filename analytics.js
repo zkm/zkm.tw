5406b0287932d4725938856f8469a275a63e0ff5
Zach Schneider <me@zachschneider.com>
2026-06-02 08:52:19 -0500

    Update production build for deployment

--- analytics.js
+++ analytics.js
@@ -1,11 +1,11 @@
 window.dataLayer = window.dataLayer || [];
 window.gtag =
-  window.gtag ||
-  function gtag() {
-    window.dataLayer.push(arguments);
-  };
+    window.gtag ||
+    function gtag() {
+        window.dataLayer.push(arguments);
+    };
 
 window.gtag('js', new Date());
 window.gtag('config', 'G-2J7SWPGLE4', {
-  anonymize_ip: true,
+    anonymize_ip: true,
 });
